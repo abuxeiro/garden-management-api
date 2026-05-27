a85eefef3f66e3ecbbc62dc5580721dace288186
Álvaro Trillo <abuxeiro@gmail.com>
2026-05-27 17:51:04 +0200

    Initial Spring Boot application setup

---
+++ src/main/java/garden_management_api/GardenManagementApiApplication.java
@@ -0,0 +1,13 @@
+package garden_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/garden_management_api/GardenManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package garden_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
